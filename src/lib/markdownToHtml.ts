18283158ccf268f1367fcc67f74cef180bf32cab
minxd95 <newobj95@gmail.com>
2023-11-29 01:41:15 +0900

    구문강조 추가

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,13 @@
-import { remark } from 'remark'
-import html from 'remark-html'
+import { remark } from "remark";
+import html from "remark-html";
+import { rehype } from "rehype";
+import rehypeHighlight from "rehype-highlight";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
-  return result.toString()
+  const raw = (await remark().use(html).process(markdown)).toString();
+  const result = await rehype()
+    .data("settings", { fragment: true })
+    .use(rehypeHighlight)
+    .process(raw);
+  return result.toString();
 }
